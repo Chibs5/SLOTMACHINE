57040bf8b52273259f24d3210e9b3b4d6b575da3
Chibwe Chungu Musendeka <76573206+Chibs5@users.noreply.github.com>
2025-06-02 14:34:29 +0200

    Update main.py
    updated the deposit function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 def depsoit():
     while True:
         amount = input("Enter the amount to deposit: ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Invalid input. Please enter a valid amount.")
+
+    return amount
